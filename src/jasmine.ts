e6d80e90e10b7ad2bd69bab4442f9894e04784a7
tjh <tjh@test.com>
2023-06-19 14:08:55 +0800

    Improved the tick function to support setTimeout

--- src/jasmine.ts
+++ src/jasmine.ts
@@ -8,15 +8,15 @@ export function renderHook<R>(fn: (...args: any) => R) {
     }
     let dispose = render(Test as any, test2)
     return {
-        result: { get current() :R { return fnReturn } },
+        result: { get current(): R { return fnReturn } },
         rerender: function () {
             dispose = render(Test as any, test2)
         },
         unmount: function () {
             dispose()
-        
+
+        }
     }
-}
 }
 
 export function act<T extends (...args: any) => any>(fn: T) {
@@ -37,53 +37,54 @@ export function mockSetInterval() {
 export function mockClearInterval() {
     spyOn(globalThis, 'clearInterval')
 }
-const ftick = (fn: ReturnType<typeof spyOn>, spyFunction: ReturnType<typeof spyOn>) =>(ms: number) => {
-    if (!fn.calls.any()) {
+
+const ftick = (setSpyFunction: ReturnType<typeof spyOn>, clearSpyFunction: ReturnType<typeof spyOn>, fn?: () => void) => (ms: number) => {
+    const setSpyCalls = setSpyFunction.calls
+    const clearSpyCalls = clearSpyFunction.calls
+    if (!setSpyCalls.any()) {
         return
     }
 
-    const callback = fn.calls.mostRecent().args[0] as Function
-    const milli = fn.calls.mostRecent().args[1] as number
-    for(let ids of spyFunction.calls.all()){
-        if(fn.calls.mostRecent().returnValue as any !== ids.args[0]){
-            break
-        }
-        else{
-            return
-        }
-    }
+    const callback = setSpyCalls.mostRecent().args[0] as Function
+    const milli = setSpyCalls.mostRecent().args[1] as number
+    const dispose = clearSpyCalls.all().some(id => //checks whether the timeout id is removed
+        setSpyCalls.mostRecent().returnValue as any === id.args[0]
+    )
+    if (!dispose) {
         if (ms >= milli) {
-            for (let i = 0; i < ms / milli ; i++) {
+            for (let i = 0; i < ms / milli; i++) {
                 callback()
             }
+            fn?.()
         }
-
+    }
 }
 export function installInterval() {
-    const clearInterval = spyOn(globalThis, 'clearInterval') 
-    const fn = spyOn(globalThis, 'setInterval').and.returnValues( Math.floor(Math.random() * 1000) as any, Math.floor(Math.random() * 1000) as any)
-    const tick = ftick(fn, clearInterval) 
+    const setSpy = spyOn(globalThis, 'setInterval').and.returnValues(Math.floor(Math.random() * 1000) as any, Math.floor(Math.random() * 1000) as any)
+    const clearSpy = spyOn(globalThis, 'clearInterval')
+    const tick = ftick(setSpy, clearSpy)
     return { tick }
 }
-export function installTimer() {
-    const clearTimeout = spyOn(globalThis, 'clearTimeout') 
-    const fn = spyOn(globalThis, 'setTimeout').and.returnValues( Math.floor(Math.random() * 1000) as any, Math.floor(Math.random() * 1000) as any)
-    const tick = ftick(fn, clearTimeout) 
+export function installTimeout() {
+    const setSpy = spyOn(globalThis, 'setTimeout').and.returnValues(Math.floor(Math.random() * 1000) as any, Math.floor(Math.random() * 1000) as any)
+    const clearSpy = spyOn(globalThis, 'clearTimeout')
+    const tick = ftick(setSpy, clearSpy, () => globalThis.clearTimeout(setSpy.calls.mostRecent().returnValue)
+    )
     return { tick }
 }
 
-    export const fireEvent = {
-        click: <T extends HTMLElement>(e: T) => {
-            e.dispatchEvent(new MouseEvent('click'))
-        },
-        keyDown: <T extends HTMLElement>(e: T) => {
-            e.dispatchEvent(new KeyboardEvent('keydown', { 'key': 'a' }));
-        },
-        mouseEnter: <T extends HTMLElement>(e: T) => {
-            e.dispatchEvent(new MouseEvent('mouseenter'));
-        },
-        mouseLeave: <T extends HTMLElement>(e: T) => {
-            e.dispatchEvent(new MouseEvent('mouseleave'));
-        },
+export const fireEvent = {
+    click: <T extends HTMLElement>(e: T) => {
+        e.dispatchEvent(new MouseEvent('click'))
+    },
+    keyDown: <T extends HTMLElement>(e: T) => {
+        e.dispatchEvent(new KeyboardEvent('keydown', { 'key': 'a' }));
+    },
+    mouseEnter: <T extends HTMLElement>(e: T) => {
+        e.dispatchEvent(new MouseEvent('mouseenter'));
+    },
+    mouseLeave: <T extends HTMLElement>(e: T) => {
+        e.dispatchEvent(new MouseEvent('mouseleave'));
+    },
 
-    }
+}
